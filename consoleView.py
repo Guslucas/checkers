1472ab0f82f05e001b515227bc3f313c44069b5d
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-02-22 21:32:21 -0300

    Switched index order

--- consoleView.py
+++ consoleView.py
@@ -1,15 +1,19 @@
 class ConsoleView:
+    LETTERS = 'ABCDEFGH'
+    
     def showCheckers(matrix):
         linha = '  '
-        for letter in 'ABCDEFGH':
-            linha += f' {letter} '
+        for col_idx in '12345678':
+            linha += f' {col_idx} '
         print(linha)
 
         for i in range(0, 8):
             linha = ''
             for j in range(0, 8):
                 linha += ConsoleView.renderPiece(matrix[i][j], i, j)
-            print(str(i+1) + ' ' + linha)
+            
+            line_idx = ConsoleView.LETTERS[i]
+            print(line_idx + ' ' + linha)
 
     def renderPiece(piece, i, j):
         if piece == 1:
